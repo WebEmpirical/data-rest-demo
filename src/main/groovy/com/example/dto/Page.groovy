908bd492dcf859ff3579a502643d58e15300867e
hjaber <hatem@7079.net>
2015-11-11 12:01:56 -0500

    commented Page.groovy pagination object

--- src/main/groovy/com/example/dto/Page.groovy
+++ src/main/groovy/com/example/dto/Page.groovy
@@ -2,6 +2,8 @@ package com.example.dto
 
 import org.springframework.hateoas.PagedResources.PageMetadata;
 
+// custom page object that extends PageMetadata
+// PageMetadata is what is used in PagedResources to return page information
 class Page extends PageMetadata {
 	
 	long size
@@ -14,6 +16,7 @@ class Page extends PageMetadata {
 		this.size = size
 		this.totalElements = totalElements
 		this.number = number
+		// we will calculate this value and return the totalPages
 		this.totalPages = (Long)Math.ceil( ( ( this.totalElements - (this.totalElements % this.size) ) / this.size ) + ( (this.totalElements % this.size > 0) ? 1 : 0 ) )
 	}	
 

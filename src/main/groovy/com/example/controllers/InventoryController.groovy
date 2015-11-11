664cb1c71cb0610c892816d6bc4662ece3733b40
hjaber <hatem@7079.net>
2015-11-11 15:41:55 -0500

    fixed page numbering need to throw an error if you select a page greater than what's available

--- src/main/groovy/com/example/controllers/InventoryController.groovy
+++ src/main/groovy/com/example/controllers/InventoryController.groovy
@@ -2,7 +2,7 @@ package com.example.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.data.web.PagedResourcesAssembler
 import org.springframework.hateoas.EntityLinks
@@ -46,15 +46,18 @@ class InventoryController implements ResourceProcessor<Resource<Inventory>> {
 			@RequestParam("name") String name,
 			@RequestParam(value="page",required=false,defaultValue="1") String page,
 			@RequestParam(value="size",required=false,defaultValue="20") String size,
-			@RequestParam(value="sort",required=false,defaultValue="_NONE_") String sort,
-			Pageable pageable
+			@RequestParam(value="sort",required=false,defaultValue="_NONE_") String sort
 	) {
 		// this is the link that will be part of our HAL/JSON documentation
 		Link link = new Link(entityLinks.linkFor(Inventory.class, "name","page","size","sort").toString() + "/search/categoryName{?name,page,size,sort}").withSelfRel()
 		// we're actually returning 2 values from the service, assign them to the temp variable for later use
-		def (resultList,totalRecords) = inventoryService.inventoryByCategoryName(name,Integer.parseInt(page),Integer.parseInt(size),sort)
+		def (p,s) = [Integer.parseInt(page),Integer.parseInt(size)]
+		def (resultList,totalRecords) = inventoryService.inventoryByCategoryName(name,p,s,sort)
+		println "****************************************"
+		println totalRecords
+		println "****************************************"
 		// returning the response as a PagedResources using the PagedResourcesAssembler to build the first,pref,next,last _links
-		return new ResponseEntity<PagedResources>(pagedResourcesAssembler.toResource(new PageImpl<InventoryDetail>(resultList,pageable,totalRecords),link),HttpStatus.OK)
+		return new ResponseEntity<PagedResources>(pagedResourcesAssembler.toResource(new PageImpl<InventoryDetail>(resultList,new PageRequest(p-1,s),totalRecords),link),HttpStatus.OK)
 	}
 	
 	// this is where we would put _links for individual "items" to be embedded and made available from them
--- src/main/groovy/com/example/services/InventoryService.groovy
+++ src/main/groovy/com/example/services/InventoryService.groovy
@@ -67,10 +67,16 @@ class InventoryService {
 		// define the actual page number here dynamically so that we have flexibility
 		// page numbers are 0 based indexes
 		int number = (page-1)*size
+		println "****************************************"
+		println number
+		println "****************************************"
 		// this is the totalRecords from our cnt query
 		Long totalRecords = em.createQuery(cnt).setParameter('name',name).getSingleResult()
 		// get the results and pass in our parameter(s) along with the page number and result size
 		List<InventoryDetail> results = em.createQuery(qry).setParameter('name',name).setFirstResult(number).setMaxResults(size).getResultList()
+		println "****************************************"
+		println results.size()
+		println "****************************************"
 		// return 2 items: results and totalRecords
 		return [results,totalRecords]
 	}
